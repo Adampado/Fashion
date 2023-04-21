f9aa12e2d27544308f0ac9dd567ab9e75874729a
Adam Njikam <njikamadammn@gmail.com>
2023-04-21 18:14:11 +0200

    function to convert images to binary

--- prototyp.py
+++ prototyp.py
@@ -1,49 +1,35 @@
-# -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+Adam Njikam
 """
-import io
 import os
 import sqlite3
-from PIL import Image 
 
 # Location of the images
-path = "Image"
+default_path = "Image"
 images = []
-# image_binary = bytearray(3)
 image_binary = []
-image_binary1 = []
+# image_binary = bytearray(3)
 
-# Get all names of images in the dir 
-if os.path.exists(path): 
-    for filename in os.listdir(path):
-        if filename.endswith("jpg") or filename.endswith("jpeg"):
-            images.append(filename)
+# Get images from given path and save as bytearray
+def image_to_binary(dir=default_path):
 
-# Read the images as binary data
-index = 0
-# for i in images:
-with open(path + '/' + images[0]) as f:
-    image_binary = f.read()
-     #   index += 1
-with open(path + '/' + images[1], 'rb') as f1:
-    image_binary1 = f1.read()
+    # Get all names of images in the dir 
+    if os.path.exists(dir): 
+        for filename in os.listdir(dir):
+            if filename.endswith("jpg") or filename.endswith("jpeg"):
+                images.append(filename)
+    # Read the images as binary data
+    index = 0
+    for i in images:
+        with open(dir + '/' + images[index], 'rb') as f:
+            image_binary.append(bytearray(f.read()))
+            index += 1
 
-
-# Convert the image data to binary format
-img = Image.open
-buf = io.BytesIO()
-img.save(buf, format='JPEG')
-img_data = buf.getvalue()
-
+# --------------------------------------------------------
 # Connect to database (create it if it doesn't exist)
 conn = sqlite3.connect('fashionPrototyp.db')
-
 # Create a cursor object to execute SQL statements
 cursor = conn.cursor()
-
 # Create a table with some columns
 create_table_query = '''CREATE TABLE IF NOT EXISTS sample 
                     (Id INTEGER PRIMARY KEY,
@@ -55,16 +41,26 @@ create_table_query = '''CREATE TABLE IF NOT EXISTS sample
                      data BLOB NOT NULL, 
                      "Product Description" TEXT)'''
 cursor.execute(create_table_query)
+# --------------------------------------------------------
 
+new_row_query = "INSERT INTO sample ('Image File Name', 'Outfit Type', Color, Brand, Price, data) VALUES (?,?,?,?,?,?)" 
+get_image_sql = "SELECT data FROM sample WHERE Id = ?"
+# cursor.execute(new_row_query, ('asos design','Casual', 'Marine blue', 'Asos', 45, sqlite3.Binary(image_binary),))
 
+# --------------------------------------------------------
+# cursor.execute(get_image_sql, (1,))
+# result = cursor.fetchone()[0]
+# img_data = bytes(result)
 
-# Insert some data into the table
-cursor.execute('''INSERT INTO sample ('Image File Name', 'Outfit Type', Color, Brand, Price, data) 
-               VALUES ('asos design','Casual', 'Marine blue', 'Asos', 45, ?)''', (image_binary[0]))
-cursor.execute("INSERT INTO sample (Image File Name, Outfit Type, Color, Brand, Price, data) VALUES ('pierre cardin','Casual', 'Pink', 'Zarlando', 57, image_binary1)")
+# Write the binary image data to a file
+# with open('retrieved_image.jpg', 'wb') as f:
+    # f.write(img_data)
+# --------------------------------------------------------
 
+    
 # Commit changes and close the connection
-conn.commit()
+cursor.close()
+# conn.commit()
 conn.close()
 
 '''
