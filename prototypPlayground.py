9f14dc61aa150d65655aa1bf86d617b41717ec2c
Adam Njikam <njikamadammn@gmail.com>
2023-04-22 09:11:13 +0200

    first commit after adding .gitignore file

--- prototypPlayground.py
+++ prototypPlayground.py
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
 """
-Adam Njikam
+Created on Sat Apr 22 01:02:56 2023
+
+@author: adamnjikam
 """
+
 import os
 import sqlite3
 
@@ -21,37 +25,42 @@ def image_to_binary(dir=default_path):
                 images.append(filename)
     # Read the images as binary data
     index = 0
-    for i in images:
-        with open(dir + '/' + images[index], 'rb') as f:
-            image_binary.append(bytearray(f.read()))
+    for i in range(len(images)):
+        print("iteration number", i)
+        with open(dir + '/' + images[i], 'rb') as f:
+            image_binary.insert(i, bytearray(f.read()))
+           # image_binary.append(bytearray(f.read()))
             index += 1
 
 image_to_binary()
 # --------------------------------------------------------
+
 # Connect to database (create it if it doesn't exist)
-conn = sqlite3.connect('fashionPrototyp.db')
+conn = sqlite3.connect('fashionPrototypTest.db')
+
 # Create a cursor object to execute SQL statements
 cursor = conn.cursor()
+
 # Create a table with some columns
 create_table_query = '''CREATE TABLE IF NOT EXISTS sample 
                     (Id INTEGER PRIMARY KEY,
-                     "Image File Name" TEXT, 
-                     "Outfit Type" TEXT,
-                     Color TEXT,
-                     Brand TEXT,
-                     Price INTEGER,
-                     Description TEXT,
-                     data BLOB NOT NULL, 
-                     "Product Description" TEXT)'''
+                      "Image File Name" TEXT, 
+                      "Outfit Type" TEXT,
+                      Color TEXT,
+                      Brand TEXT,
+                      Price INTEGER,
+                      Description TEXT,
+                      data BLOB, 
+                      )'''
 cursor.execute(create_table_query)
 # --------------------------------------------------------
 
-insert_query = "INSERT INTO sample ('Image File Name', 'Outfit Type', Color, Brand, Price, data) VALUES (?,?,?,?,?,?,?)"
+insert_query = "INSERT INTO sample ('Image File Name', 'Outfit Type', Color, Brand, Price, 'Description', data) VALUES (?,?,?,?,?,?,?)"
  
 # get_image_sql = "SELECT data FROM sample WHERE Id = ?"
 
 cursor.execute(insert_query, ('asos_design.jpeg','Casual', 'Marine blue', 'Asos', 45, 'Striped shirt', sqlite3.Binary(image_binary[0]),))
-cursor.execute(insert_query, ('athletic_tshirt.jpeg',	'Athletic',	'Blue',	'Nike', 29.99, 'Dri-FIT, short sleeve', sqlite3.Binary(image_binary[1]),))
+cursor.execute(insert_query, ('athletic_tshirt.jpeg', 'Athletic', 'Blue', 'Nike', 29.99, 'Dri-FIT, short sleeve', sqlite3.Binary(image_binary[1]),))
 cursor.execute(insert_query, ('formal_black_suit.jpg', 'Formal', 'Black',  'Hugo', 299.99, 'Slim fit, two-piece',sqlite3.Binary(image_binary[2]),))
 # cursor.execute(insert_query, ('casual_red_dress.jpg', 'Casual',	'Red', 'Zara', 49.99, 'A-line, floral',))
 
@@ -65,52 +74,7 @@ cursor.execute(insert_query, ('formal_black_suit.jpg', 'Formal', 'Black',  'Hugo
     # f.write(img_data)
 # --------------------------------------------------------
 
-    
 # Commit changes and close the connection
 cursor.close()
 # conn.commit()
 conn.close()
-
-'''
-# path = 'C:/Program Files/SQLiteStudio/fashion.db'
-path = 'fashion.db'
-
-if os.path.isfile(path):
-    try: 
-        conn = sqlite3.connect(path)
-
-        # Cursor to interact with database
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM Prototyp")
-
-        result = cursor.fetchall()
-        for row in result:
-            print(row)
-            
-        conn.close()
-    except sqlite3.Error as e:
-        print(e)
-else:
-    print("Database does not exist")
-    
-'''
-# ---------------------------------------------
-
-"""
-
-from PIL import Image 
-# Location of the images
-path = "imageTester"
-images = []
-
-if os.path.exists(path): 
-    for filename in os.listdir(path):
-        if filename.endswith("jpg") or filename.endswith("jpeg"):
-            images.append(filename)
-    print(images)
-n = 0
-for i in images:
-    image = Image.open(path+'/'+images[n])
-    n += 1
-    image.show()
-"""
